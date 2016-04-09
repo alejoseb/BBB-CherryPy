78f1466649558b71e68340d3cff6d3ae992d47d3
Alejandro Mera <alejandro@ip-172-31-51-92.ec2.internal>
2016-04-09 17:43:13 +0000

    Read/Write file added

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import os, os.path
 import cherrypy
-import Adafruit_BBIO.GPIO as GPIO
-import time
 
 class bbbPage(object):
     @cherrypy.expose
@@ -9,44 +7,44 @@ class bbbPage(object):
         return open('index.html')
 
 class bbbPageWebService(object):
-     exposed = False
-     def blinkLed(self):
-        for x in range(1,5):
-            GPIO.output("P9_14", GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output("P9_14", GPIO.LOW)
-            time.sleep(0.1)   
-        return
     
      exposed = True
      @cherrypy.tools.accept(media='text/plain')
      def PUT(self, x, y):
-        self.blinkLed()
+       # self.blinkLed()
         print "x: "+ str(x) +" y: " + str(y) 
-        return "x: "+ str(x) +" y: " + str(y)   
+	print "Opening the file..."
+	target = open('./log.txt', 'a')
+	target.write('X:' +  str(x) + ' ,Y:'+str(y) +'\n')
+        target.close()
+ 	return "x: "+ str(x) +" y: " + str(y)             
      
      def ON(self):
-        GPIO.output("P9_14", GPIO.HIGH)
+        #GPIO.output("P9_14", GPIO.HIGH)
         print "Led on" 
         return "Led on"    
         
      def OFF(self):
-        GPIO.output("P9_14", GPIO.LOW)
+        #GPIO.output("P9_14", GPIO.LOW)
         print "Led off" 
         return "Led off"    
     
      def BLINK(self):
-        self.blinkLed()
+        #self.blinkLed()
         print "Led BLINKING" 
         return "Led BLINKING" 
-         
+
+
+     def LEER(self):
+        print "Reading text"
+	f =open('./log.txt')
+        texto=''
+	for line in f:
+		texto=texto+line 
+        return texto        
 
 if __name__ == '__main__':
      conf = {
-        #'/': {
-        #     'tools.sessions.on': True,
-        #     'tools.staticdir.root': os.path.abspath(os.getcwd())
-        #},
         '/': {
              'tools.staticdir.root': os.path.abspath(os.getcwd())
          },
@@ -63,7 +61,7 @@ if __name__ == '__main__':
          
          
      }
-     GPIO.setup("P9_14", GPIO.OUT)
+#     GPIO.setup("P9_14", GPIO.OUT)
      webapp = bbbPage()
      webapp.generator = bbbPageWebService()
      cherrypy.config.update({'server.socket_host': '0.0.0.0'})
